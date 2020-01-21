14380c9c88d7b2d49bb43b99deef656ac743df41
Soumya Brata Dutta <58485756+sbd2309@users.noreply.github.com>
2020-01-22 01:32:59 +0530

    Updated.

--- Designer_PDF_Viewer.py
+++ Designer_PDF_Viewer.py
@@ -1,10 +1,17 @@
 n=input()
-l=[int(i) for i in input().strip().split(' ')]
+l=n.strip().split(' ')
+pdf=input()
 al=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 d={}
 x=0
 for i in l:
-    d.update({al[x]:i})
+    d.update({al[x]:int(i)})
     x+=1
+height=0
+for i in pdf:
+    for key,value in d.items():
+        if i==key:
+            if height<value:
+                height=value
 
-print(d)
+print(len(pdf)*height)
